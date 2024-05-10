5b4bcd24a569d59856f5234223d1be438c5824a6
kiryu <13matheusilva@gmail.com>
2024-05-09 23:18:11 -0300

    relogin random position add

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,17 @@ const buttonYes = document.querySelector('.button-yes')
 const buttonNo = document.querySelector('.button-no')
 const sectionQuestion = document.querySelector('.question-section')
 const sectionResponse = document.querySelector('.response-section')
+const relogin = document.querySelector('.relogin')
 
 buttonNo.addEventListener('mouseover', () =>{
     buttonNo.style.position = "absolute"
     buttonNo.style.top = Math.random()*90 + "vh"
-    buttonNo.style.right = Math.random()*90 + "vw"
+    buttonNo.style.left = Math.random()*90 + "vw"
+})
+
+relogin.addEventListener('mouseover', ()=>{
+    relogin.style.top = Math.random()*90 + "vh"
+    relogin.style.left = Math.random()*90 + "vw"
 })
 
 buttonYes.addEventListener('click', () =>{
